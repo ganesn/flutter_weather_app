00c3e7f786c697b97fb78a15b516022d78428b4c
ganesn <ganesanmdev@gmail.com>
2023-03-11 01:59:26 +0530

    city selection added for location fetching

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:weather_app/model/weather_model.dart';
 import 'package:weather_app/services/weather_api_client.dart';
 import 'package:weather_app/views/additonal_information.dart';
 import 'package:weather_app/views/current_weather.dart';
+import 'package:csc_picker/csc_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,33 +13,22 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
-  
 
-  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:HomePage()
-    
-    
-    );
+    return MaterialApp(home: HomePage());
   }
 }
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
- 
-
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   WeatherApiClient client = WeatherApiClient();
-
 
   //  @override
   // void initState(){
@@ -46,49 +36,177 @@ class _HomePageState extends State<HomePage> {
   // client.getCurrentWeather("Chennai");
   // }
 
-  Weather? data ;
+  Weather? data;
+  String countryValue = "";
+  String stateValue = "";
+  String cityValue = "";
+  String address = "";
   Future<void> getData() async {
+    data = await client
+        .getCurrentWeather(cityValue == '' ? "Tiruppur" : cityValue);
+  }
 
-    data = await client.getCurrentWeather("tiruppur");
-
-  }  @override
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: Color(0xFFf9f9f9),
-      appBar: AppBar(
-        backgroundColor: Color(0xFFf9f9f9),
-        elevation: 0.0,
-        title: Text("Weather App",style: TextStyle(color: Colors.black)),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: (){},
-          icon: Icon(Icons.menu),
-          color: Colors.black,
-        ),
-      ), 
-      body: FutureBuilder(future:getData(),
-      builder: (context,snapshot){
-         if(snapshot.connectionState == ConnectionState.done){
-          return Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          currentWeather(Icons.wb_sunny_rounded, "${data!.temp}", "${data!.cityName}"),
-          SizedBox(height:60.0),
-          Text("Additional Informations",style: TextStyle(fontSize: 24.0, color:Color(0xdd212121),fontWeight: FontWeight.bold),),
-          Divider(),
-          SizedBox(
-            height: 20.0,
-          ),
-          additionalInformation("${data!.wind}", "${data!.humidity}", "${data!.pressure}", "${data!.feels_like}"),
+    GlobalKey<CSCPickerState> _cscPickerKey = GlobalKey();
 
-        ],
-      );
-  
-        }else if (snapshot.connectionState == ConnectionState.waiting){
-          return Center(child: CircularProgressIndicator(),);
-        } 
-        return Container();
-      },));
+    return Scaffold(
+        backgroundColor: Color(0xFFf9f9f9),
+        appBar: AppBar(
+          backgroundColor: Color(0xFFf9f9f9),
+          elevation: 0.0,
+          title: Text("Weather App", style: TextStyle(color: Colors.black)),
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu),
+            color: Colors.black,
+          ),
+        ),
+        body: FutureBuilder(
+          future: getData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return SingleChildScrollView(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CSCPicker(
+                    currentCity: cityValue,
+                    currentCountry: countryValue,
+                    currentState: stateValue,
+
+                    ///Enable disable state dropdown [OPTIONAL PARAMETER]
+                    showStates: true,
+
+                    /// Enable disable city drop down [OPTIONAL PARAMETER]
+                    showCities: true,
+
+                    ///Enable (get flag with country name) / Disable (Disable flag) / ShowInDropdownOnly (display flag in dropdown only) [OPTIONAL PARAMETER]
+                    flagState: CountryFlag.DISABLE,
+
+                    ///Dropdown box decoration to style your dropdown selector [OPTIONAL PARAMETER] (USE with disabledDropdownDecoration)
+                    dropdownDecoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.white,
+                        border:
+                            Border.all(color: Colors.grey.shade300, width: 1)),
+
+                    ///Disabled Dropdown box decoration to style your dropdown selector [OPTIONAL PARAMETER]  (USE with disabled dropdownDecoration)
+                    disabledDropdownDecoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.grey.shade300,
+                        border:
+                            Border.all(color: Colors.grey.shade300, width: 1)),
+
+                    ///placeholders for dropdown search field
+                    countrySearchPlaceholder: "Country",
+                    stateSearchPlaceholder: "State",
+                    citySearchPlaceholder: "City",
+
+                    ///labels for dropdown
+                    countryDropdownLabel: "Country",
+                    stateDropdownLabel: "State",
+                    cityDropdownLabel: "City",
+
+                    ///Default Country
+                    ///defaultCountry: CscCountry.India,
+
+                    ///Country Filter [OPTIONAL PARAMETER]
+                    // countryFilter: [CscCountry.India,CscCountry.United_States,CscCountry.Canada],
+
+                    ///Disable country dropdown (Note: use it with default country)
+                    //disableCountry: true,
+
+                    ///selected item style [OPTIONAL PARAMETER]
+                    selectedItemStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
+
+                    ///DropdownDialog Heading style [OPTIONAL PARAMETER]
+                    dropdownHeadingStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold),
+
+                    ///DropdownDialog Item style [OPTIONAL PARAMETER]
+                    dropdownItemStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
+
+                    ///Dialog box radius [OPTIONAL PARAMETER]
+                    dropdownDialogRadius: 10.0,
+
+                    ///Search bar radius [OPTIONAL PARAMETER]
+                    searchBarRadius: 10.0,
+
+                    ///triggers once country selected in dropdown
+                    onCountryChanged: (value) {
+                      setState(() {
+                        ///store value in country variable
+                        print('Country Value $value!');
+                        countryValue = value;
+                      });
+                    },
+
+                    ///triggers once state selected in dropdown
+                    onStateChanged: (value) {
+                      setState(() {
+                        ///store value in state variable
+                        ///
+                        print('Statte Value $value!');
+                        stateValue = value ?? "";
+                      });
+                    },
+
+                    ///triggers once city selected in dropdown
+                    onCityChanged: (value) {
+                      setState(() {
+                        ///store value in city variable
+                        cityValue = value ?? "";
+                        print('City Value ${value ?? ""} ');
+                        getData();
+                      });
+                    },
+
+                    ///Show only specific countries using country filter
+                    // countryFilter: ["United States", "Canada", "Mexico"],
+                  ),
+                  currentWeather(Icons.wb_sunny_rounded, "${data!.temp}",
+                      "${data!.cityName}"),
+                  SizedBox(height: 60.0),
+                  Text(
+                    "Additional Informations",
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        color: Color(0xdd212121),
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Divider(),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  additionalInformation("${data!.wind}", "${data!.humidity}",
+                      "${data!.pressure}", "${data!.feels_like}"),
+                ],
+              ));
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (!snapshot.hasData) {
+              return Container(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+            return Container();
+          },
+        ));
   }
 }
-
