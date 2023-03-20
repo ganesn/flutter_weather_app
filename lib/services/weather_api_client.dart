02664b1e876be9def83221acf7c5d56bea40813e
Ganesan M <127085838+ganesn@users.noreply.github.com>
2023-03-20 12:01:21 +0530

    Fixing Security Issues

--- lib/services/weather_api_client.dart
+++ lib/services/weather_api_client.dart
@@ -10,7 +10,7 @@ import 'package:http/http.dart' as http;
 class WeatherApiClient {
   Future<Weather>? getCurrentWeather(String? location) async{
     print("Called Api");
-    var endpoint = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$location&appid=371146d0a4a068d699a6bc7c54c2e7fc&units=metric");
+    var endpoint = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$location&appid=[API_KEY_HERE]&units=metric");
     var response = await http.get(endpoint);
     var body = jsonDecode(response.body);
     print(Weather.fromJSON(body));
